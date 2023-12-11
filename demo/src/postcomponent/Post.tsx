da86d0a029cb37d5be0acfb9fbc4942280e3fce4
mehmet dedeler <mehmet.dedeler@ug.bilkent.edu.tr>
2023-12-11 23:57:18 +0300

    post finished. it will be entegrated to homepage

--- demo/src/postcomponent/Post.tsx
+++ demo/src/postcomponent/Post.tsx
@@ -1,28 +1,96 @@
 // Post.tsx
 import React from 'react';
 import './Post.css';
+import { useState } from 'react';
 
 const Post: React.FC = () => {
+  const user = {}; // Placeholder for user object from backend
+
+  // Empty function to handle report user action
+  const handleReportUser = (user: any) => {
+    // Placeholder for logic to report a user
+  };
+
+  // Empty function to handle unfollow user action
+  const handleUnfollowUser = (user: any) => {
+    // Placeholder for logic to unfollow a user
+  };
+
+
+  const [currentImageIndex, setCurrentImageIndex] = useState(0);
+  const images = [
+    '../../public/dedeler.jpg',
+    '../../public/homedark.png',
+    '../../public/gündüz.jpg',
+  ];
+  const [socialMediaPost, setSocialMediaPost] = useState({
+    username: 'marc.zucker31dsfasdfasdasdsadasdsadasdsadasdasdsadasd',
+    content: 'Elon I will beat you !!! thjsd hflkaskfdlssadasdasdsdasdasdasasdasdas adşkfjsdşahf jadshfjadshfjşla dshfjasdhfjashdfljasdhfljk hsdalasdasdsadasdas',
+    time: '12 Min',
+    likeCount: 1125, // Initialize like count
+    liked: false, // State to track if the user has liked the post
+  });
+  const toggleLike = () => {
+    setSocialMediaPost((prevPost) => ({
+      ...prevPost,
+      liked: !prevPost.liked,
+      likeCount: prevPost.liked ? prevPost.likeCount - 1 : prevPost.likeCount + 1,
+    }));
+  };
+  const [showOptions, setShowOptions] = useState(false);
+  const toggleOptions = () => {
+    setShowOptions(prevShow => !prevShow);
+  };
+  const goPrev = () => {
+    setCurrentImageIndex((prevIndex) => (prevIndex > 0 ? prevIndex - 1 : images.length - 1));
+  };
+
+  const goNext = () => {
+    setCurrentImageIndex((prevIndex) => (prevIndex + 1) % images.length);
+  };
+  const handleComment = () => {
+    // Placeholder for comment logic
+  };
+
+  // Empty function for handling share action
+  const handleShare = () => {
+    // Placeholder for share logic
+  };
+
   return (
     <div className="post-container">
       <div className="post-header">
-        <img className="profile-pic" src="profile_pic_url.jpg" alt="Profile" />
+        <img className="profile-pic" src="../../public/dedeler.jpg" alt="Profile" />
         <div className="username-time">
-          <div className="username">marc.zucker31axy</div>
-          <div className="time-posted">12 Min</div>
+          <div className="username">{socialMediaPost.username}</div>
+          <div className="time-posted">{socialMediaPost.time}</div>
         </div>
-        <div className="more-options">...</div>
+        <div className="more-options" onClick={toggleOptions}>
+          <div className="more-options-icon">...</div>
+          {showOptions && (
+            <div className="options-panel">
+            <button className="option-button" onClick={() => handleReportUser(user)}>Report User</button>
+            <button className="option-button" onClick={() => handleUnfollowUser(user)}>Unfollow User</button>
+          </div>
+          )}
+        </div>
       </div>
       <div className="post-content">
-        <p className="post-text">Elon I will beat you !!!</p>
-        <img className="post-image" src="post_image_url.jpg" alt="Post content" />
+        <p className="post-text">{socialMediaPost.content}</p>
+        <div className="post-image-carousel">
+          <img className="post-image" src={images[currentImageIndex]} alt="Post content" />
+          <button className="carousel-arrow left" onClick={goPrev}>‹</button>
+          <button className="carousel-arrow right" onClick={goNext}>›</button>
+        </div>
       </div>
       <div className="post-actions">
-        <span className="like-count">1125 likes</span>
         <div className="post-buttons">
-          <button className="like-button">Like</button>
-          <button className="comment-button">Comment</button>
-          <button className="share-button">Share</button>
+        <button className={`like-button ${socialMediaPost.liked ? 'liked' : ''}`} onClick={toggleLike}>
+            👍
+            <span className="like-count">{socialMediaPost.likeCount}</span>
+          </button>
+          <button className="comment-button" onClick={handleComment}>💬 Comment</button>
+          <button className="share-button" onClick={handleShare}>🔗 Share</button>
         </div>
       </div>
     </div>
