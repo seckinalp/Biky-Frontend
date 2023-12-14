ba9f4ca40c92162e5c5fd639fae632ff926a78ed
mehmet dedeler <mehmet.dedeler@ug.bilkent.edu.tr>
2023-12-14 14:39:09 +0300

    hata düzeltme

---
+++ demo/src/profilecomponent/editprofile/EditProfile.tsx
@@ -0,0 +1,16 @@
+// EditProfile.tsx
+import React from 'react';
+import './EditProfile.css';
+
+const EditProfile: React.FC = () => {
+  return (
+    <div className="edit-profile-container">
+      {/* Form elements for editing the profile will go here */}
+      <h1>Edit Profile</h1>
+      {/* Add form fields for name, surname, username, bio, etc. */}
+      {/* Add a 'Save' button to submit changes */}
+    </div>
+  );
+};
+
+export default EditProfile;
