4131bc35dad4ffa3f39a9bb336c37e4b2ce9dce5
seckinalp <alp.kargi@ug.bilkent.edu.tr>
2023-12-14 20:40:29 +0300

    Settings- Report Problem finsihed, also new button style

--- demo/src/settingsComponent/Settings.tsx
+++ demo/src/settingsComponent/Settings.tsx
@@ -4,14 +4,28 @@ import './Settings.css';
 function Settings() {
   const [statusMessage, setStatusMessage] = useState('');
   const [isVisible, setIsVisible] = useState(true); // state to control visibility
+  const [isReportVisible, setIsReportVisible] = useState(false); // state to control report form visibility
+  const [reportMessage, setReportMessage] = useState(''); // state to store the report message
 
   const handleReport = () => {
-    setStatusMessage('Problem reported!');
+    
     // Additional logic for reporting a problem
   };
-
+  const handleReportOpen = () => {
+    setIsReportVisible(true); // Show the report form
+  };
+  const handleReportSubmit = () => {
+    
+    setReportMessage(''); // Clear the report message
+    setIsReportVisible(false); // Hide the report form
+    // Here you would handle the report submission to your backend
+  };
+  const handleGoBack = () => {
+    setIsReportVisible(false); // Hide the report form to show settings list
+    setStatusMessage(''); // Optionally clear any status messages
+  };
   const handleLogout = () => {
-    setStatusMessage('Logged out successfully!');
+    
     // Additional logic for logging out
   };
 
@@ -21,21 +35,44 @@ function Settings() {
 
   // Return null if the component should not be visible
   if (!isVisible) return null;
-
+  const containerClass = isReportVisible ? "settings-container large" : "settings-container";
   return (
-    <div className="settings-container">
+    <div className={containerClass}>
       <div className="settings-header">
-        <span>Settings</span>
-        <button className="close-button" onClick={handleClose}>✖</button>
+        <span>{isReportVisible ? "Report a Problem" : "Settings"}</span>
+        {isReportVisible ? (
+             <button className="back-button" onClick={handleGoBack}>
+             <span className="material-icons">arrow_back</span>
+           </button>
+        ) : (
+          <button className="close-button" onClick={handleClose}>✖</button>
+        )}
       </div>
-      <ul className="settings-list">
-        <li className="settings-item" onClick={handleReport}>
-          <span className="icon">🚩</span> Report a problem
-        </li>
-        <li className="settings-item" onClick={handleLogout}>
-          <span className="icon">↩️</span> Log out
-        </li>
-      </ul>
+      {isReportVisible ? (
+        <div className="report-form">
+  <div className="textarea-container">
+    <textarea
+      value={reportMessage}
+      onChange={(e) => setReportMessage(e.target.value)}
+      placeholder="Describe the problem..."
+    ></textarea>
+  </div>
+  <div className="button-container">
+    <button className='btn' onClick={handleReportSubmit}>Submit Report</button>
+  </div>
+</div>
+  
+      ) : (
+        <ul className="settings-list">
+          <li className="settings-item" onClick={handleReportOpen}>
+            <span className="icon">🚩</span> Report a problem
+          </li>
+          <li className="settings-item" onClick={handleLogout}>
+            <span className="icon">↩️</span> Log out
+          </li>
+        </ul>
+      )}
+      {statusMessage && <div className="status-message">{statusMessage}</div>}
     </div>
   );
 }
