97222afddf5843ed0b632145acf9e7188d4bb037
seckinalp <alp.kargi@ug.bilkent.edu.tr>
2023-12-17 00:34:15 +0300

    button changes

--- demo/src/createPostComponent/CreatePost.tsx
+++ demo/src/createPostComponent/CreatePost.tsx
@@ -109,7 +109,7 @@ const CreatePost: React.FC<CreatePostProps> = ({ onClose, onSubmit }) => {
 <div className="create-post">
       <div className="modal-header">
         <h2 className="modal-title">Create a Post</h2>
-        <button className="close-button" onClick={handleClose}>&times;</button>
+        <button className="close-button" onClick={handleClose}></button>
       </div>
       <form onSubmit={handleSubmit}>
       <div className="post-type-buttons">
